ae3a0a9d4cb8da76f4f69615f3f89c9180554815
Mohamed el Amine Benallouch <amine.benallouch@assistme.io>
2020-05-09 17:23:36 +0000

    add test case for the UseCase module

---
+++ usecase/src/test/java/com/benallouch/usecases/GetArticlesUseCaseTest.kt
@@ -0,0 +1,41 @@
+package com.antonioleiva.usecases
+
+import com.benallouch.data.entity.NewsResponse
+import com.benallouch.data.repository.ArticlesRepository
+import com.benallouch.usecase.GetArticlesUseCase
+import com.benallouch.usecases.mockedArticle
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetArticlesUseCaseTest {
+
+    @Mock
+    lateinit var articlesRepository: ArticlesRepository
+
+    lateinit var getPopularArticles: GetArticlesUseCase
+
+    @Before
+    fun setUp() {
+        getPopularArticles = GetArticlesUseCase(articlesRepository)
+    }
+
+    @Test
+    fun `invoke calls articles repository`() {
+        runBlocking {
+
+            val articles = arrayListOf(mockedArticle.copy(articleId = 1))
+            whenever(articlesRepository.getArticles(1)).thenReturn(NewsResponse(null, 100, articles))
+
+            val result = getPopularArticles.invoke(1)
+
+            Assert.assertEquals(articles, result.articles)
+        }
+    }
+}
---
+++ usecase/src/test/java/com/benallouch/usecases/MockedArticle.kt
@@ -0,0 +1,18 @@
+package com.benallouch.usecases
+
+import com.benallouch.data.entity.Article
+import com.benallouch.data.entity.NewsSource
+import java.time.Instant
+import java.util.*
+
+val mockedArticle = Article(
+        0,
+        NewsSource(UUID.randomUUID().toString(), "ESPN"),
+        "author",
+        "title",
+        "description",
+        "url",
+        "urlToImage",
+        Date.from(Instant.now()),
+        "content"
+)
