05a32ceb5ee6b3619aa492099462053937e56e32
Mohamed el Amine Benallouch <amine.benallouch@assistme.io>
2020-05-10 20:36:34 +0000

    wrap the api key and url inside gradle build files

--- app/src/main/java/com/benallouch/yunar/ui/Constants.kt
+++ app/src/main/java/com/benallouch/yunar/ui/Constants.kt
@@ -1,8 +1,14 @@
 package com.benallouch.yunar.ui
 
+//view related constants
 const val VIEW_TYPE_ITEM = 0
 const val VIEW_TYPE_LOADING = 1
 const val VIEW_TYPE_ITEM_HEADER = 2
 const val ITEMS_PER_FETCH = 21
-const val EXTRA_ARTICLE_URL="ARTICLE_URL"
+const val EXTRA_ARTICLE_URL = "ARTICLE_URL"
+
+//api related constants
+const val API_KEY = "apiKey"
+const val BASE_URL = "baseUrl"
+
 
